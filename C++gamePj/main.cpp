9cda05a8c097737a81d226ba72673f651837cd83
dayeone1 <s2312@e-mirim.hs.kr>
2024-12-01 00:21:36 +0900

    레시피 랜덤 생성

--- C++gamePj/main.cpp
+++ C++gamePj/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <functional>
 #include <string>
+#include <random>
 
 using namespace std;
 
@@ -20,7 +21,6 @@ int score = -1;
 
 class Button {
 public:
-    Button() {};
     Button(const string& normalImagePath, const string& hoverImagePath,
            float x, float y, float x1, float y1, function<void()> onClick)
         : onClick(onClick) {
@@ -48,16 +48,6 @@ public:
         hoverSprite.setTexture(buttonClickImg);
         hoverSprite.setPosition(x, y);
     }
-
-    //Button(sf::Texture buttonImg, float x, float y, function<void()> onClick)
-    //    : buttonImg(buttonImg), onClick(onClick) {
-    //    this->buttonImg = buttonImg;
-    //    sprite.setTexture(this->buttonImg);
-    //    sprite.setPosition(x, y);
-    //    hoverSprite.setTexture(this->buttonImg); // 클릭 이미지와 일반 이미지가 같으면 같은 텍스처 사용
-    //    hoverSprite.setPosition(x, y);
-    //}
-
 
     void draw(sf::RenderWindow& window) {
         if (isHovered) {
@@ -99,7 +89,9 @@ public:
     EndingScreen(int num)
         :button_start("img/start/startButton_default.png", "img/start/startButton_start.png", 1086, 450, 960, 380, []() {
         cout << "Start button clicked!" << endl;
-        screen_num = screen_number::Main; // 상태 변경
+        // score을 -1로 만들어 main에서 reset 진행되게 함
+        score = -1;
+        screen_num = screen_number::Main; // 화면 변경
         })
     {
         if (num == screen_number::Happy_Ending)
@@ -154,93 +146,126 @@ private:
 class MainScreen : public Screen {
 public:
     MainScreen()
+        // 각 오브제 클릭 영역 설정
         : button_area{
-            Button("img/main/obj_box/ice_box.png", "", 84, 169, [this]() {
+            Button(objString[0], "", 84, 169, [this]() {
                 cout << "Ice Box clicked!" << endl;
                 event = 0;
                 isFollowingMouse = true; // 마우스를 따라다니는 것을 활성화
-            }),
-            Button("img/main/obj_box/ice_box.png", "", 85, 260, [this]() {
+            }), 
+            Button(objString[0], "", 85, 260, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 1;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/ice_box.png", "", 85, 358, [this]() {
+            Button(objString[0], "", 85, 358, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 2;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/cream_box.png", "", 202, 160, [this]() {
+            Button(objString[1], "", 202, 160, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 3;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/cream_box.png", "", 202, 255, [this]() {
+            Button(objString[1], "", 202, 255, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 4;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/cream_box.png", "", 202, 358, [this]() {
+            Button(objString[1], "", 202, 358, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 5;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/fruit_box.png", "", 65, 497, [this]() {
+            Button(objString[2], "", 65, 497, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 6;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/fruit_box.png", "", 191, 497, [this]() {
+            Button(objString[2], "", 191, 497, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 7;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/cup_box.png", "", 387, 90, [this]() {
+            Button(objString[3], "", 387, 90, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 8;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/bread_box.png", "", 387, 269, [this]() {
+            Button(objString[4], "", 387, 269, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 9;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/topping_box.png", "", 392, 365, [this]() {
+            Button(objString[5], "", 392, 365, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 10;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/topping_box.png", "", 484, 365, [this]() {
+            Button(objString[5], "", 484, 365, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 11;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/topping_box.png", "", 576, 365, [this]() {
+            Button(objString[5], "", 576, 365, [this]() {
                 cout << "Another Box clicked!" << endl;
                 event = 12;
                 isFollowingMouse = true;
             }),
-            Button("img/main/obj_box/trash_box.png", "", 1014, 502, [this]() {
+            Button(objString[6], "", 1014, 502, [this]() {
                 cout << "Another Box clicked!" << endl;
                 isFollowingMouse = false;
                 event = -1;
             }),
-            Button("img/main/obj_box/bench_box.png", "", 703, 269, [this]() {
+
+            // 작업대
+            Button(objString[7], "", 703, 269, [this]() {
                 cout << "Another Box clicked!" << endl;
                 isFollowingMouse = false;
+
+                // 1. 조합순서 확인
+               
+                // 1-2. 틀렸을경우 event = -1 하고 배열에는 추가하지 않음
+
+                // 2. event 확인 후 이미지 띄우기 
+
+                // 3. 파르페가 완성되었을때 예시와 같은지 확인
+                if (cook_num == 7 && event != -1) {
+                    int chk = 1;
+                    for (int i = 0; i < 9; i++)
+                        if (cook[i] != recipe[i])
+                            chk = 0;
+                    // 4. 맞는지 틀린지에 따라 표정 변화 후 파르페 배열 초기화, 0.5초 유지 이후 
+                    if (chk) {
+
+                    }
+                    else {
+
+                    }
+       
+                }
+
                 event = -1;
             })
         }
     {
+
+        // 오브제 이미지
+
+        
         // 배경 및 기타 요소 초기화
         backgroundTexture.loadFromFile("img/background/main_back.png");
         backgroundSprite.setTexture(backgroundTexture);
+
         tableTexture.loadFromFile("img/main/table.png");
         tableSprite.setTexture(tableTexture);
         tableSprite.setPosition(690, 278);
+
         messageTexture.loadFromFile("img/main/message.png");
         messageSprite.setTexture(messageTexture);
         messageSprite.setPosition(690, 10);
+
         objTexture.loadFromFile("img/main/obj.png");
         objSprite.setTexture(objTexture);
 
@@ -254,6 +279,9 @@ public:
             followTexture[i].loadFromFile("img/main/obj_mouse/follow_" + to_string(i) + ".png");
             followSprite[i].setTexture(followTexture[i]);
             followSprite[i].setPosition(-100, -100);
+
+            cookTexture[i].loadFromFile("img/main/obj_cook/cook_" + to_string(i) + ".png");
+            cookSprite[i].setTexture(cookTexture[i]);
         }
     }
 
@@ -267,6 +295,11 @@ public:
                 // 각 버튼을 그리기
                 for (int i = 0; i < 15; i++) {
                     button_area[i].draw(window);
+                }
+
+                for (int i = 0; i < 9; i++) {
+                    cookSprite[recipe[cook_order[i]]].setPosition(recipeXY[0][cook_order[i]], recipeXY[1][cook_order[i]]);
+                    window.draw(cookSprite[recipe[cook_order[i]]]);
                 }
 
                 // 마우스를 따라다니는 이미지가 활성화 됐을 경우 그리기
@@ -294,27 +327,108 @@ public:
                 }
             }
 
+            void reset() {
+                score = 0;
+                emotion = 0;
+                event = -1;
+                cook_num = 0;
+                isFollowingMouse = false;
+                randomRecipe();
+                resetCook();
+            }
+
+            void randomRecipe() {
+                recipe[0] = 8;
+
+                // 난수 생성기 및 시드 설정
+                std::random_device rd; // 진정한 난수 생성기
+                std::default_random_engine generator(rd()); // 시드를 rd()로 설정
+                std::uniform_int_distribution<int> distribution(0, 13); // 0 ~ 13
+
+                for (int i = 1; i < 9; i++) {
+                    int ran = distribution(generator);
+
+                    // 레시피 순서에 맞지 않는 재료일 경우 다시 생성, 맞으면 저장
+                    if (ran >= cook_arr[i][0] && ran <= cook_arr[i][2]) {
+                        recipe[i] = ran;
+                        cout << ran << endl;
+                    }
+                    else {
+                        i--; continue;
+                    }
+                }
+            }
+
+            void resetCook() {
+                for (int i = 0; i < 9; i++)
+                    cook[i] = -1;
+            }
+
 private:
-    int emotion = 0;
-    int event = -1;
-    int score = 0;
+    int emotion = 0; // 캐릭터의 표정
+    int event = -1; // 오브제
+    int cook_num = 0; // 조리 진행도
     bool isFollowingMouse = false; // 마우스를 따라다닐지 여부
 
+
+    // 각 오브제 클릭 영역
+    string objString[8] = {
+        "img/main/obj_box/ice_box.png",
+        "img/main/obj_box/cream_box.png",
+        "img/main/obj_box/fruit_box.png",
+        "img/main/obj_box/cup_box.png",
+        "img/main/obj_box/bread_box.png",
+        "img/main/obj_box/topping_box.png",
+        "img/main/obj_box/trash_box.png",
+        "img/main/obj_box/bench_box.png",
+    };
+
+    // 배경
     sf::Texture backgroundTexture;
     sf::Sprite backgroundSprite;
+
+    // 각 음식 등 오브젝트
     sf::Texture objTexture;
     sf::Sprite objSprite;
+
+    // 테이블(작업대 & 스코어판)
     sf::Texture tableTexture;
     sf::Sprite tableSprite;
+
+    // 말풍선
     sf::Texture messageTexture;
     sf::Sprite messageSprite;
 
+    // 캐릭터
     sf::Texture charTexture[3];
     sf::Sprite charSprite[3];
 
-    sf::Texture followTexture[13]; // 마우스를 따라다닐 이미지 텍스처
-    sf::Sprite followSprite[13];    // 마우스를 따라다닐 이미지 스프라이트
+    // 음식 작업대 이미지
+    sf::Texture cookTexture[13];
+    sf::Sprite cookSprite[13];
 
+    // 음식 작업대 이미지 위치 배열
+    int cookXY[2][11] = { {788, 804, 804, 804, 804, 804, 804, 804, 798, 788},{281, 399, 378, 378, 355, 333, 333, 298, 288} };
+
+    // 음식 레시피 이미지 위치 배열
+    int recipeXY[2][11] = { {763, 777, 777, 777, 777, 777, 777, 771, 761} ,{86, 157, 136, 136, 113, 91, 91, 56, 46} };
+
+    // 레시피 배열
+    int recipe[9];
+
+    // 작업대 배열
+    int cook[9]; 
+
+    // 작업대 들어갈 수 있는 음식의 종류(순서) 배열
+    int cook_arr[9][3] = {{8,8,8}, {9,9,9}, {6,7,7}, {3, 4, 5}, {9,9,9}, {6,7,7}, {3, 4, 5}, {0, 1, 2}, {10, 11, 12} };
+
+    // 이미지 생성 순서
+    int cook_order[9] = { 1, 4, 3, 6, 2, 5, 0, 7, 8 };
+
+    // 마우스를 따라다닐 이미지
+    sf::Texture followTexture[13]; 
+    sf::Sprite followSprite[13];
+    
     // 버튼 배열 선언
     Button button_area[15];
 };
@@ -350,7 +464,6 @@ public:
     StartScreen()
         :button_start("img/start/startButton_default.png", "img/start/startButton_start.png", 855, 270, 740, 210, []() {
             cout << "Start button clicked!" << endl;
-            score = 0;
             screen_num = screen_number::Main; // 상태 변경
         }),
         button_rule("img/start/startButton_default1.png", "img/start/startButton_rule.png", 705, 270, 590, 210, []() {
@@ -470,6 +583,12 @@ int main() {
             start_screen.draw(window);
             break;
         case screen_number::Main:
+            // 메인스크린 초기화
+            if (score == -1) {
+                main_screen.reset();
+                cout << "main screen reset" << endl;
+            }
+               
             main_screen.draw(window);
 
             if (!timerStarted) {
